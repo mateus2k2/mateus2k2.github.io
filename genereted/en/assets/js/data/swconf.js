b23c7c36c686e44b43cde50b747c83f16c311b7e
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-06-23 09:11:03 -0300

    add

--- genereted/en/assets/js/data/swconf.js
+++ genereted/en/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1719080397',resources: [
+    cacheName: 'chirpy-1719144600',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
