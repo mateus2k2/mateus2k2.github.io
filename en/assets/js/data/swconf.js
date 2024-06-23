004cafd1143a611c5937b46540ccae296dd1aeb7
mateus2k2 <mateus2k2@users.noreply.github.com>
2024-06-23 18:54:58 +0000

    deploy: 2da5d5c9512034003d1d961e3c48cc25b405b635

--- en/assets/js/data/swconf.js
+++ en/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1719168547',resources: [
+    cacheName: 'chirpy-1719168895',resources: [
       '/en/assets/css/jekyll-theme-chirpy.css',
       '/en/',
       
